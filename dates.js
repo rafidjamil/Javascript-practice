8838ac614596f49b26d8eeaf4ac63cc627bd0263
Rafid Jamil <rafidjamil001@gmail.com>
2026-01-20 09:06:57 -0800

    Dates

---
+++ dates.js
@@ -0,0 +1,33 @@
+//dates 
+//date is the object instance.
+// month 0 sy start hote hn js may 
+// let myDate = new Date();
+// console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+
+// let myDate02 = new Date(2002,6,31)
+// let myDate02 = new Date(2002,6,31 , 5,15)
+// let myDate02 = new Date("11-08,2004")
+// console.log(myDate02.toLocaleString());
+
+// let myDateStamp = Date.now()
+// console.log(Math.floor(Date.now()/1000));
+
+// console.log(myDateStamp);
+
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.getMonth()+1);
+console.log(myDate.getDay());
+
+// myDate.toLocaleString('default',{
+//     weekday: "long",
+
+// })
+
+
+
