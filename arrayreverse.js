23ab1d6ed5aabe708d1b8104ba769fd1168941a2
Rafid Jamil <rafidjamil001@gmail.com>
2026-01-20 09:05:12 -0800

    Reverse the array

---
+++ arrayreverse.js
@@ -0,0 +1,17 @@
+let num=[2,4,6,8,10,12,14,16,18,20]
+
+
+
+
+for(i=num.length-1;i>=0;i--){
+
+        console.log(num[i]);
+        
+        
+    
+        
+    
+    
+}
+// console.log(max);
+// console.log("123456789")
