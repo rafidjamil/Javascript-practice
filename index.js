39f617f924b534aaefcd8c85cfc3fd13acec5ec1
Rafid Jamil <rafidjamil001@gmail.com>
2026-01-21 07:11:45 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+num = 1;
+num2 = 2.5;
+name = "Hello, World!";
+isActive = true;
+asd = null;
+qwe = undefined;
+list = [1, 2, 3];
+
+// key value pair
+std = {
+    age: 12,
+    name: "ali",
+    class: {
+        sec: "A",
+        rollno: 123,
+
+    },
+    marks: [12,43,30, {
+        subject: "math",
+        marks: 100
+    }],
+    pass: false,
+};
+
+
+msg = name
+
+
+console.log(list);
+console.log(num2);
+console.log(name);
+console.log(msg);
+console.log(std);
+console.log(std.name);
+console.log(std.class);
+console.log(std.class.sec);
